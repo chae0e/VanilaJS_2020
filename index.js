f7c842e6edfeb17695833ee4d2f973b9fd928cd3
chae0e <62542804+chae0e@users.noreply.github.com>
2020-08-28 17:02:06 +0900

    #1.10 노마드코더

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
-let a = 222;
+const a = 222;
 let b = a - 199;
-a = 7;
 console.log(b, a);
+/*이것들은 Array
+const daysOfWeek = [
+  "월요일",
+  "화요일",
+  "수요일",
+  "목요일",
+  "금요일",
+  "토요일",
+  "일요일",
+];
+console.log(daysOfWeek);
+console.log(daysOfWeek[2]);
+*/
+const chaeInfo = {
+  name: "Chae",
+  age: 28,
+  gender: "female",
+  lovesArt: true,
+  favMovies: [
+    { name: "아버지와 이토씨", genre: "feature film" },
+    { name: "파프리카", genre: "animation" },
+  ],
+};
+console.log(chaeInfo.favMovies[0]);
+console.log(chaeInfo.favMovies[0].genre);
